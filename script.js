625967e345b935aa215ebd865ea651ca16357e23
eloiluizh <eloiluizh@gmail.com>
2025-02-01 15:11:44 -0300

    Design changes

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggle(IconId, IsHovered) {
+    const icon = document.getElementById(IconId)
+
+    if (IsHovered) {
+        const classesToRemove = IconId.includes("insta") ? ["fa-brands", "fa-instagram"] : ["fa-brands", "fa-github"]
+        icon.classList.remove(...classesToRemove)
+        const classesToAdd = IconId.includes("github") ? ["fa-brands", "fa-square-github"] : ["fa-brands", "fa-square-instagram"]
+        icon.classList.add(...classesToAdd)
+    } else {
+        const classesToRemove = IconId.includes("insta") ? ["fa-brands", "fa-square-instagram"] : ["fa-brands", "fa-square-github"]
+        icon.classList.remove(...classesToRemove)
+        const classesToAdd = IconId.includes("insta") ? ["fa-brands", "fa-instagram"] : ["fa-brands", "fa-github"]
+        icon.classList.add(...classesToAdd)
+    }
+}
+console.error();
+console.log("Já que ta fuçando no codigo, aproveita e me segue no insta @oluizzn")
+
+// condição ? valorSeVerdadeiro : valorSeFalso
